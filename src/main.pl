da7dd1eace31dab4939449633f66d5df4a8068d6
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2022-08-16 11:36:45 -0300

    telas main

--- src/main.pl
+++ src/main.pl
@@ -1,13 +1,19 @@
-% :- include('produto.pl').
-% :- include('carrinho.pl').
 :- use_module(produto, [printProdutos/0, putProduto/3, removeProduto/1, getProduto/2]).
 :- use_module(carrinho, [printCarrinho/0, putProdutoNoCarrinho/4, removeProdutoDoCarrinho/1]).
 
 main():-
     write('\e[2J'),
-    print('1 - User'),nl,
-    print('2 - Admin'),nl,
-    print('0 - Sair'),nl,
+
+    writeln('        +-+-+-+-+ +-+-+-+-+-+-+'),
+    writeln('        |E|a|s|y| |M|a|r|k|e|t|'),
+    writeln('        +-+-+-+-+ +-+-+-+-+-+-+'),
+    writeln(' ===================================== '),
+    writeln('          1- CLIENTE                   '),
+    writeln('          2- ADMINISTRADOR             '),
+    writeln('          0- SAIR                      '),
+    writeln(' ===================================== '),nl,
+
+    write('Digite sua opcao: '),
     read(Option),
     mainOption(Option),
     main().
@@ -17,40 +23,57 @@ mainOption(2):- admin().
 mainOption(0):- halt(0).
 
 
-
 % User ----------------------------
 user():-
     write('\e[2J'),
-    print('1 - Listar Produtos.'),nl,
-    print('2 - Adicionar produto no carrinho.'),nl,
-    print('3 - Remover produto do carrinho.'),nl,
-    print('0 - Voltar para a tela inicial.'),nl,
+    writeln('             +-+-+-+-+-+-+-+'),
+    writeln('             |C|L|I|E|N|T|E|'),
+    writeln('             +-+-+-+-+-+-+-+'),nl,
+    writeln(' ===================================== '),
+    writeln('      1- LISTAGEM DE PRODUTOS          '),
+    writeln('      2- LISTAGEM DE CARRINHO          '),
+    writeln('      3- ADD PRODUTO CARRINHO          '),
+    writeln('      4- REMOVER PRODUTO DO CARRINHO   '),
+    writeln('      5- REALIZAR COMPRA               '),
+    writeln('      0- SAIR                          '),
+    writeln(' ===================================== '),nl,
+    write('Digite sua opcao: '),
     read(Option),
     userOption(Option),
     user().
 
 userOption(1):- printProdutos().
-userOption(2):-
+userOption(2):- printCarrinho().
+userOption(3):-
     print('Digite o ID:'),nl,
     read(Id),
     print('Digite a quantidade:'),nl,
     read(Quantidade),
     getProduto(Id, row(_, Nome, Preco, Categoria)),
     putProdutoNoCarrinho(Nome, Preco, Categoria, Quantidade).
-userOption(3):-
+userOption(4):-
     print('Digite o ID:'),nl,
     read(Id),
     removeProdutoDoCarrinho(Id).
+userOption(5):-
+    halt.
 userOption(0):- main().
 % ---------------------------------
 
 % Admin ---------------------------
 admin():-
     write('\e[2J'),
-    print('1 - Cadastrar Produto.'),nl,
-    print('2 - Excluir Produto.'),nl,
-    print('3 - Listar Produtos.'),nl,
-    print('0 - Voltar para a tela inicial.'),nl,
+    writeln('                 +-+-+-+               '),
+    writeln('                 |A|D|M|               '),
+    writeln('                 +-+-+-+               '),nl,
+    writeln(' ===================================== '),
+    writeln('      1- CADASTRAR PRODUTO             '),
+    writeln('      2- EXCLUIR PRODUTO               '),
+    writeln('      3- LISTAR PRODUTOS               '),
+    writeln('      0- SAIR                          '),
+    writeln(' ===================================== '),nl,
+    write('Digite sua opcao: '),
+
     read(Option),
     adminOption(Option),
     admin().
